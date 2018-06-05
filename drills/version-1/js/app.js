822292a719c7510c4ae7141d1922662f94b62513
Danny Pifer <piferdg@gmail.com>
2018-06-05 14:42:18 -0600

    Adding code for version 3 drill
    -m "Adding version 3 drill code"#

--- drills/version-1/js/app.js
+++ drills/version-1/js/app.js
@@ -1,8 +1,44 @@
 var currentPlayer = "X";
 var board = ["", "", "", "", "", "", "", "", ""];
+var gameBoard = document.getElementById('gameboard')
+var messageElement = document.getElementsByClassName('message')[0]
 
-/* Your code here /*
- *
-*/
+function createGame() {
+    for (let i = 0; i < 9; i++) {
+        var cell = document.createElement('div')
+        cell.setAttribute('id', i)
+        cell.classList.add('square')
 
-createGame();
+        if (i === 0 || i === 1 || i === 3 || i === 4) {
+            cell.classList.add('b-border', 'r-border')
+        }
+        else if (i === 2 || i === 5) {
+            cell.classList.add('b-border')
+        }
+        else if (i === 6 || i === 7) {
+            cell.classList.add('r-border')
+        }
+
+        gameBoard.appendChild(cell)
+        addClickListener(cell)
+    }
+}
+createGame()
+
+function displayMessage(mess) {
+    if (mess) {
+        messageElement.innerHTML = mess
+    }
+    else {
+        messageElement.innerHTML = 'Current player: ' + currentPlayer
+    }
+}
+
+function changeMessage() {
+    messageElement.setAttribute('class', 'end-message message')
+}
+
+function makeMove(square, index) {
+    board[index] = currentPlayer
+    square.innerHTML = currentPlayer
+}
